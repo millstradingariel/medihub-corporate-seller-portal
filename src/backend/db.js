b8668d93d0ce72bd8632ee13f33e4e2bcf75bcaa
millstradingariel <millstrading.ariel@gmail.com>
2026-02-11 18:14:42 +1100

    medihubcommit0211v2

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'medihub',
-    waitForConnections: true,
+  user: process.env.DB_USER,   
+  password: process.env.DB_PASSWORD,   
+  database: process.env.DB_NAME,            
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, 
+  waitForConnections: true,
 });
 
 pool.query('SELECT 1')
