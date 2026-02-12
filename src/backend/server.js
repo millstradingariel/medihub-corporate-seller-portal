101cb325f9c264e9a1814f884cf32f61360f818f
millstradingariel <millstrading.ariel@gmail.com>
2026-02-12 17:19:15 +1100

    Fix dotenv import in verifyToken

--- src/backend/server.js
+++ src/backend/server.js
@@ -103,7 +103,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env._PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment variables loaded:`);
