b792527af96d9c3998c176916e5941b4ad324b16
millstradingariel <millstrading.ariel@gmail.com>
2026-02-13 13:50:53 +1100

    02132026revision

---
+++ src/frontend/vite.config.111.ts
@@ -0,0 +1,16 @@
+// // frontend/vite.config.ts
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   root: "src/app",   // index.html location
+//   build: {
+//     outDir: "../../dist",  // relative to root
+//     emptyOutDir: true,
+//   },
+//   plugins: [react()],
+//   base: '/',
+//   server: {
+//     port: 3000,
+//   },
+// })
